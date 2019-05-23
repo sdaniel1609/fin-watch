7a4ad4968df42210a0e2eaa968f248afeb84f158
Simon Daniel <sdaniel1609@gmail.com>
2019-05-23 06:11:17 +0100

    added financial report to lookup page

---
+++ src/app/model/BaseFinancials.ts
@@ -0,0 +1,28 @@
+export interface BaseFinancials {
+  standardized_financials: StandardizedFinancialsItem[];
+  fundamental: Fundamental;
+}
+interface StandardizedFinancialsItem {
+  data_tag: DataTag;
+  value: number;
+}
+interface DataTag {
+  id: string;
+  name: string;
+  tag: string;
+  parent: string | null;
+  sequence: number;
+  factor: string;
+  balance: string | null;
+  unit: string;
+}
+interface Fundamental {
+  id: string;
+  statement_code: string;
+  fiscal_year: number;
+  fiscal_period: string;
+  type: string;
+  start_date: string;
+  end_date: string;
+  filing_date: string;
+}
